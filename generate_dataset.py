3488711494242ff70281cae3ea735fdbc02f1fc4
anshuln <anshulnasery@gmail.com>
2019-10-17 02:04:24 +0530

    Cleanup

--- generate_dataset.py
+++ generate_dataset.py
@@ -56,36 +56,12 @@ def generate_npy_files(train_file_dir_list=[],test_file_dir='images'):
 			# file.create_dataset('data', data=np.concatenate(img_batch,axis=0))
 			# file.create_dataset('label', data=np.concatenate(labels_batch,axis=0))
 			# file.close()
-			img_batch = [list() for i in range(len(train_file_dir_list))]   # TODO shuffle training data
+			img_batch = [list() for i in range(len(train_file_dir_list))]   
 			labels_batch = []
 			labels_images_batch = []
 			idx_h5 = idx_h5 + 1
 
 
-	# for dirname, _, filenames in os.walk(base_dir):
-	# 	for filename in filenames:
-	# 		if ".jpg" in filename:
-	# 			for dir_idx,dir in enumerate(train_file_dir_list):
-	# 				img_batch[dir_idx].append(cv2.imread(os.path.join(dirname.replace(base_dir,dir), filename)))
-	# 			# print(dirname,filename)
-	# 			labels_batch[0].append(cv2.imread(os.path.join(dirname.replace(base_dir,test_file_dir), filename))) 
-	# 			labels_batch[1].append(one_hot(labels.index(dirname.split('/')[-1]),len(labels)))
-	# 			idx_img+=1
-	# 			if ((idx_img + 1) % batch_size == 0):# or idx_img == len(images) - 1:
-	# 				#TODO save h5 files
-	# 				filename_h5_data = 'data/' + '%d.npy' % (idx_h5)
-	# 				filename_h5_label = 'label/' + '%d.npy' % (idx_h5)
-	# 				print('Saving {}.npy...'.format(idx_h5))
-	# 				# filelist_h5.write('./%d.h5\n' % (idx_h5))
-	# 				np.save(filename_h5_data,np.array(img_batch))
-	# 				np.save(filename_h5_label,np.array(labels_batch))
-	# 				# file = h5py.File(filename_h5, 'w')
-	# 				# file.create_dataset('data', data=np.concatenate(img_batch,axis=0))
-	# 				# file.create_dataset('label', data=np.concatenate(labels_batch,axis=0))
-	# 				# file.close()
-	# 				img_batch = [[]]*len(train_file_dir_list)   # TODO shuffle training data
-	# 				labels_batch = [[]]*2
-	# 				idx_h5 = idx_h5 + 1
 
 
-generate_npy_files()
+
