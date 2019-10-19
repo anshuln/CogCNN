Fixes error which caused no gradients

generate_dataset.py
@@ -16,7 +16,7 @@ def one_hot(label_index,len_labels):
 	one_hot = [0]*len_labels
 	one_hot[label_index] = 1
 	return one_hot
-def generate_npy_files(train_file_dir_list=[],test_file_dir='images'):
+def generate_npy_files(train_file_dir_list=['amazon_silhouette','amazon_texture/images'],test_file_dir='images'):
 	base_dir = train_file_dir_list[0]
 	labels = get_labels(test_file_dir) 
 	idx_img = 0
@@ -24,22 +24,22 @@ def generate_npy_files(train_file_dir_list=[],test_file_dir='images'):
 	img_batch = [list() for i in range(len(train_file_dir_list))]   # TODO shuffle training data
 	labels_batch = []
 	labels_images_batch = []
-	batch_size = 50
+	batch_size = 16
 	image_paths = []
 	for l in labels:
 		dir = "{}/{}".format(base_dir,l)
 		for file in os.listdir(dir):
-		    if file.endswith(".jpg"):
-		    	image_paths.append("{}/{}".format(l,file))
+			if file.endswith(".jpg"):
+				image_paths.append("{}/{}".format(l,file))
 	shuffle(image_paths)
 	print(labels)
 	for path in image_paths:
 		for dir_idx in range(len(train_file_dir_list)):
 			dire = train_file_dir_list[dir_idx]
-			img_batch[dir_idx].append(cv2.imread("{}/{}".format(dire,path))/255.0)
+			img_batch[dir_idx].append(cv2.resize((cv2.imread("{}/{}".format(dire,path))/255.0),(64,64)))
 			#print(len(img_batch),len(img_batch[0]),len(img_batch[1]))
 			#print(dir_idx,"{}/{}".format(dire,path))
-		labels_images_batch.append(cv2.imread("{}/{}".format(test_file_dir,path))/255.0)
+		labels_images_batch.append(cv2.resize((cv2.imread("{}/{}".format(test_file_dir,path))/255.0),(64,64)))
 		labels_batch.append(one_hot(labels.index(path.split('/')[0]),len(labels)))
 		idx_img+=1
 		if ((idx_img + 1) % batch_size == 0):# or idx_img == len(images) - 1:
@@ -56,12 +56,36 @@ def generate_npy_files(train_file_dir_list=[],test_file_dir='images'):
 			# file.create_dataset('data', data=np.concatenate(img_batch,axis=0))
 			# file.create_dataset('label', data=np.concatenate(labels_batch,axis=0))
 			# file.close()
-			img_batch = [list() for i in range(len(train_file_dir_list))]   
+			img_batch = [list() for i in range(len(train_file_dir_list))]   # TODO shuffle training data
 			labels_batch = []
 			labels_images_batch = []
 			idx_h5 = idx_h5 + 1
 
 
+	# for dirname, _, filenames in os.walk(base_dir):
+	# 	for filename in filenames:
+	# 		if ".jpg" in filename:
+	# 			for dir_idx,dir in enumerate(train_file_dir_list):
+	# 				img_batch[dir_idx].append(cv2.imread(os.path.join(dirname.replace(base_dir,dir), filename)))
+	# 			# print(dirname,filename)
+	# 			labels_batch[0].append(cv2.imread(os.path.join(dirname.replace(base_dir,test_file_dir), filename))) 
+	# 			labels_batch[1].append(one_hot(labels.index(dirname.split('/')[-1]),len(labels)))
+	# 			idx_img+=1
+	# 			if ((idx_img + 1) % batch_size == 0):# or idx_img == len(images) - 1:
+	# 				#TODO save h5 files
+	# 				filename_h5_data = 'data/' + '%d.npy' % (idx_h5)
+	# 				filename_h5_label = 'label/' + '%d.npy' % (idx_h5)
+	# 				print('Saving {}.npy...'.format(idx_h5))
+	# 				# filelist_h5.write('./%d.h5\n' % (idx_h5))
+	# 				np.save(filename_h5_data,np.array(img_batch))
+	# 				np.save(filename_h5_label,np.array(labels_batch))
+	# 				# file = h5py.File(filename_h5, 'w')
+	# 				# file.create_dataset('data', data=np.concatenate(img_batch,axis=0))
+	# 				# file.create_dataset('label', data=np.concatenate(labels_batch,axis=0))
+	# 				# file.close()
+	# 				img_batch = [[]]*len(train_file_dir_list)   # TODO shuffle training data
+	# 				labels_batch = [[]]*2
+	# 				idx_h5 = idx_h5 + 1
 
 
-
+generate_npy_files()

model.py
@@ -10,20 +10,20 @@ class MultiTaskModel(Sequential):
 		#image_shape is the shape of 1 image for reconstruction
 		#TODO - kwargs support for segnet initializations
 		super(MultiTaskModel, self).__init__()
-		self.num_inputs = num_inputs	
+		self.num_inputs = num_inputs    
 		self.image_shape = image_shape
 		self.segnets = []
-		self.trainableVariables = []	#Not to be confused with trainable_variables, which is read-only
+		self.trainableVariables = []    #Not to be confused with trainable_variables, which is read-only
 		for i in range(num_inputs):
 			self.segnets.append(SegNet())
 		print("Image_Shape",image_shape)
 		self.reconstruct_image = Sequential([Flatten(),Dense(1000),
 				Dense(image_shape[0]*image_shape[1]*image_shape[2],activation='relu')])
 		self.predict_label = Sequential([Flatten(),Dense(1000),
-				Dense(num_labels,activation='softmax')])	#The loss function uses softmax, final preds as well
+				Dense(num_labels,activation='softmax')])    #The loss function uses softmax, final preds as well
 
 	def setTrainableVariables(self):
-		for i in range(self.num_inputs):	
+		for i in range(self.num_inputs):    
 			print("On segnet",i)
 			self.trainableVariables += self.segnets[i].trainable_variables()
 		self.trainableVariables += self.reconstruct_image.trainable_variables
@@ -44,19 +44,21 @@ class MultiTaskModel(Sequential):
 			enc,rec = self.segnets[i+1].call(X[i+1])
 			enc = tf.expand_dims(enc,1)
 			encoded_reps = tf.concat([encoded_reps,enc],axis=1)
-			result.append(rec)	#Appending the reconstructed result to return 
-		print(encoded_reps.shape)
-		result.append(tf.reshape(self.reconstruct_image(encoded_reps),(batch,h,w,c)))	#Appending final image
-		result.append(self.predict_label(encoded_reps))		#Appending final labels
+			result.append(rec)  #Appending the reconstructed result to return 
+		#print(encoded_reps.shape)
+		result.append(tf.reshape(self.reconstruct_image(encoded_reps),(batch,h,w,c)))   #Appending final image
+		result.append(self.predict_label(encoded_reps))     #Appending final labels
 		return result
 
 	def loss_reconstruction(self,X,Y):
+		# print(X.shape,Y.shape)
 		#Pixel-wise l2 loss
-		return  tf.math.reduce_sum(tf.math.reduce_sum(tf.math.reduce_sum((X-Y)**2,
-			axis=-1),axis=-1),axis=-1,keepdims=True)	#see if keepdims is required
+		# return  tf.math.reduce_sum(tf.math.reduce_sum(tf.math.reduce_sum((X-Y)**2,
+			# axis=-1),axis=-1),axis=-1,keepdims=True)    #see if keepdims is required
+		return tf.math.reduce_sum((X-Y)**2)
 
 	def loss_classification(self,X,labels):
-		return tf.keras.losses.CategoricalCrossentropy(labels,X)
+		return tf.keras.losses.CategoricalCrossentropy()(labels,X)
 
 	def train_on_batch(self,X,Y_image,Y_labels,optimizer):
 		# Y needs to be a list of [img,labels]
@@ -67,8 +69,13 @@ class MultiTaskModel(Sequential):
 			for i in range(self.num_inputs-1):
 				loss += self.loss_reconstruction(X[i+1],result[i+1])
 			loss += self.loss_reconstruction(result[self.num_inputs],Y_image)
+			# print("Loss: ",loss)
 			loss += self.loss_classification(result[self.num_inputs+1],Y_labels)
+			# print("Loss: ",loss)
 		grads = tape.gradient(loss,self.trainableVariables)
+		# for t in self.trainableVariables:
+			# print(type(t))
+		# print(len(grads),len(self.trainableVariables))
 		grads_and_vars = zip(grads, self.trainableVariables)
 		optimizer.apply_gradients(grads_and_vars)
 		return loss

multitask_segnet_tf2.py
@@ -19,15 +19,15 @@ class SegNet(Sequential):
 			filter = [64, 128, 256, 512, 512]
 
 		self.nettype = nettype
-		self.max_indices = []	#TODO can we refactor to make this a tensor?
+		self.max_indices = []   #TODO can we refactor to make this a tensor?
 
 		# defining the convolution part of encoder-decoder blocks
 		#TODO Refactor for better model making
 		self.conv_block_enc = Sequential([Sequential([self.conv_layer(filter[0]),self.conv_layer(filter[0])])])
 		self.conv_block_dec = Sequential([Sequential([self.conv_layer(filter[-2]),
 												  self.conv_layer(filter[-2]),
-												  self.conv_layer(filter[-3])])])	
-		for i in range(3):	#TODO Refactor for better model making
+												  self.conv_layer(filter[-3])])])   
+		for i in range(3):  #TODO Refactor for better model making
 			if i == 0:
 				self.conv_block_enc.add(Sequential([self.conv_layer(filter[i + 1]),
 													self.conv_layer(filter[i + 1])]))
@@ -41,7 +41,7 @@ class SegNet(Sequential):
 		self.conv_block_dec.add(Sequential([self.conv_layer(filter[-4]),
 												  self.conv_layer(filter[0])]))
 		self.conv_block_dec.add(Sequential([self.conv_layer(filter[0]),
-												  self.conv_layer(filter[channels])]))
+												  self.conv_layer(channels)]))
 
 		#Uncomment for task specific layers  (Won't be using here)  
 		# self.pred_task1 = nn.Sequential(nn.Conv2d(in_channels=filter[0], out_channels=filter[0], kernel_size=3, padding=1),
@@ -63,7 +63,7 @@ class SegNet(Sequential):
 				BatchNormalization(axis=-1),
 				ReLU(),
 				Conv2D(filters=channel, kernel_size=3, padding="valid"),
-				BatchNormalization(axis=-1),	#SANITY CHECK, see that channels are LAST!!!
+				BatchNormalization(axis=-1),    #SANITY CHECK, see that channels are LAST!!!
 				ReLU()]
 			)
 		else:
@@ -77,25 +77,26 @@ class SegNet(Sequential):
 	def call(self,X):
 		# Returns 2 tensors, one is the encoded representation, other 
 		# is the reconstruction
-		for layer in self.conv_block_enc.layers:	
+		for layer in self.conv_block_enc.layers:    
 			X = layer(X)
 			# print(layer.weights)
 			X,ind = self.down_sampling.call(X)
 			self.max_indices.append(ind)
-		X_enc = X 	#POTENTIAL BUG IF SHALLOW COPY
-		self.max_indices = self.max_indices[::-1]	#Reverse list for easier access
+		X_enc = X   #POTENTIAL BUG IF SHALLOW COPY
+		self.max_indices = self.max_indices[::-1]   #Reverse list for easier access
 		#print("INDICES______",self.max_indices)
 		for idx,layer in enumerate(self.conv_block_dec.layers):
 			# print(idx,X.shape,self.max_indices[idx].shape)
 			X = self.up_sampling.call([X,self.max_indices[idx]])
 			# print(idx,X.shape,self.max_indices[idx].shape)
 			X = layer(X)
+		# print("Segnet call shape",X.shape,X_enc.shape)
 		return [X_enc,X]
 
 	def trainable_variables(self):
 		tv = []
 		for layer in self.conv_block_enc.layers:
-			tv.append(layer.trainable_variables)
+			tv+=(layer.trainable_variables)
 		for layer in self.conv_block_dec.layers:
-			tv.append(layer.trainable_variables) 
+			tv+=(layer.trainable_variables) 
 		return tv

train.py
@@ -19,32 +19,33 @@ image_shape = (64,64,3)
 num_labels = 31
 
 
-def train(model,optimizer,epochs):	#TODO add validation, generator for datasets
-	files = []
-	for file in os.listdir(train_dir):
-	    if file.endswith(".npy"):	#Change to h5 later
-	    	files.append(file)
-	for epoch in range(epochs):
-		loss = []
-		for f in tqdm(files):
-			input_batch = np.load("{}/{}".format(train_dir,f),allow_pickle=True)
-			label_batch = np.load("{}/{}".format(label_dir,f),allow_pickle=True)
-			label_im_batch = np.load("{}/{}".format(label_im_dir,f),allow_pickle=True)
-			l = model.train_on_batch(input_batch,label_im_batch,label_batch,optimizer)
-			loss.append(l.numpy())
-		print("Epoch - {} Loss - {}".format(epoch,np.array(loss).mean()))
+def train(model,optimizer,epochs):  #TODO add validation, generator for datasets
+    files = []
+    for file in os.listdir(train_dir):
+        if file.endswith(".npy"):   #Change to h5 later
+            files.append(file)
+    for epoch in range(epochs):
+        loss = []
+        for f in tqdm(files):
+            input_batch = np.load("{}/{}".format(train_dir,f),allow_pickle=True)
+            label_batch = np.load("{}/{}".format(label_dir,f),allow_pickle=True)
+            label_im_batch = np.load("{}/{}".format(label_im_dir,f),allow_pickle=True)
+            l = model.train_on_batch(input_batch,label_im_batch,label_batch,optimizer)
+            loss.append(l.numpy())
+        print("Epoch - {} Loss - {}".format(epoch,np.array(loss).mean()))
 
 if __name__ == "__main__":
-	TF_CPP_MIN_LOG_LEVEL = 4
-	model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels)
-	d1 = np.load("{}/0.npy".format(train_dir),allow_pickle=True)
-	print(d1[:,:1].shape)
-	print("Building Model...")
-	pred = model.call(d1[:,:1])
-	print(len(pred))
-	print("Model built")
-	optimizer = tf.optimizers.Adam()
-	model.setTrainableVariables()
-	train(model,optimizer,5)
+    TF_CPP_MIN_LOG_LEVEL = 4
+    model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels)
+    d1 = np.load("{}/0.npy".format(train_dir),allow_pickle=True)
+    print(d1[:,:1].shape)
+    print("Building Model...")
+    pred = model.call(d1[:,:1])
+    for x in pred:
+        print("Shape",x.shape)	
+    print("Model built")
+    optimizer = tf.optimizers.Adam()
+    model.setTrainableVariables()
+    train(model,optimizer,5)
 
 