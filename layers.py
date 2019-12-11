c093fd9a4b3259de009dbd95a5ba991c45d58515
anshuln <anshulnasery@gmail.com>
2019-12-12 04:00:39 +0530

    Changes dimensions of latent space

--- layers.py
+++ layers.py
@@ -4,126 +4,127 @@ import tensorflow as tf
 #MAY HAVE TO REWRITE STUFF FOR TF2 COMPAT
 class MaxPool2D(Layer):
 
-    def __init__(
-            self,
-            ksize=(2, 2),
-            strides=(2, 2),
-            padding='same',
-            **kwargs):
-        super(MaxPool2D, self).__init__(autocast=False)
-        self.padding = padding
-        self.pool_size = ksize
-        self.strides = strides
+	def __init__(
+			self,
+			ksize=(2, 2),
+			strides=(2, 2),
+			padding='same',
+			**kwargs):
+		super(MaxPool2D, self).__init__(autocast=False)
+		self.padding = padding
+		self.pool_size = ksize
+		self.strides = strides
 
-    def call(self, inputs, **kwargs):
-        padding = self.padding
-        pool_size = self.pool_size
-        strides = self.strides
-        ksize = [1, pool_size[0], pool_size[1], 1]
-        padding = padding.upper()
-        strides = [1, strides[0], strides[1], 1]
-        output, argmax = tf.nn.max_pool_with_argmax(
-                inputs,
-                ksize=ksize,
-                strides=strides,
-                padding=padding)
-        argmax = tf.cast(argmax, tf.float64)
-        return [output, argmax]
+	def call(self, inputs, **kwargs):
+		padding = self.padding
+		pool_size = self.pool_size
+		strides = self.strides
+		ksize = [1, pool_size[0], pool_size[1], 1]
+		padding = padding.upper()
+		strides = [1, strides[0], strides[1], 1]
+		output, argmax = tf.nn.max_pool_with_argmax(
+				inputs,
+				ksize=ksize,
+				strides=strides,
+				padding=padding)
+		argmax = tf.cast(argmax, tf.float64)
+		return [output, argmax]
 
-    def compute_output_shape(self, input_shape):
-        ratio = (1, 2, 2, 1)
-        output_shape = [
-                dim//ratio[idx]
-                if dim is not None else None
-                for idx, dim in enumerate(input_shape)]
-        output_shape = tuple(output_shape)
-        return [output_shape, output_shape]
+	def compute_output_shape(self, input_shape):
+		ratio = (1, 2, 2, 1)
+		output_shape = [
+				dim//ratio[idx]
+				if dim is not None else None
+				for idx, dim in enumerate(input_shape)]
+		output_shape = tuple(output_shape)
+		return [output_shape, output_shape]
 
-    def compute_mask(self, inputs, mask=None):
-        return 2 * [None]
+	def compute_mask(self, inputs, mask=None):
+		return 2 * [None]
 
 
 class MaxUnpool2D(Layer):
-    def __init__(self, ksize=(2, 2), **kwargs):
-        super(MaxUnpool2D, self).__init__(autocast=False,**kwargs)
-        self.size = ksize
+	def __init__(self, ksize=(2, 2), **kwargs):
+		super(MaxUnpool2D, self).__init__(autocast=False,**kwargs)
+		self.size = ksize
 
-    def call(self, inputs, output_shape=None):
-        updates, mask = inputs[0], inputs[1]
-        mask = tf.cast(mask, 'int32')
-        input_shape = tf.shape(updates, out_type='int32')
-        #  calculation new shape
-        if output_shape is None:
-            output_shape = (
-                    input_shape[0],
-                    input_shape[1]*self.size[0],
-                    input_shape[2]*self.size[1],
-                    input_shape[3])
-        self.output_shape1 = output_shape
+	def call(self, inputs, output_shape=None):
+		updates, mask = inputs[0], inputs[1]
+		mask = tf.cast(mask, 'int32')
+		input_shape = tf.shape(updates, out_type='int32')
+		#  calculation new shape
+		if output_shape is None:
+			output_shape = (
+					input_shape[0],
+					input_shape[1]*self.size[0],
+					input_shape[2]*self.size[1],
+					input_shape[3])
+		self.output_shape1 = output_shape
 
-        # calculation indices for batch, height, width and feature maps
-        one_like_mask = tf.ones_like(mask, dtype='int32')
-        batch_shape = tf.concat(
-                [[input_shape[0]], [1], [1], [1]],
-                axis=0)
-        batch_range = tf.reshape(
-                tf.range(output_shape[0], dtype='int32'),
-                shape=batch_shape)
-        # print("SHAPE______",output_shape[3])
-        b = one_like_mask * batch_range
-        y = mask // (output_shape[2] * output_shape[3])
-        x = (mask // output_shape[3]) % output_shape[2]
-        feature_range = tf.range(output_shape[3], dtype='int32')
-        f = one_like_mask * feature_range
+		# calculation indices for batch, height, width and feature maps
+		one_like_mask = tf.ones_like(mask, dtype='int32')
+		batch_shape = tf.concat(
+				[[input_shape[0]], [1], [1], [1]],
+				axis=0)
+		batch_range = tf.reshape(
+				tf.range(output_shape[0], dtype='int32'),
+				shape=batch_shape)
+		# print("SHAPE______",output_shape[3])
+		b = one_like_mask * batch_range
+		y = mask // (output_shape[2] * output_shape[3])
+		x = (mask // output_shape[3]) % output_shape[2]
+		feature_range = tf.range(output_shape[3], dtype='int32')
+		f = one_like_mask * feature_range
 
-        # transpose indices & reshape update values to one dimension
-        updates_size = tf.size(updates)
-        indices = tf.transpose(tf.reshape(
-            tf.stack([b, y, x, f]),
-            [4, updates_size]))
-        values = tf.reshape(updates, [updates_size])
-        ret = tf.scatter_nd(indices, values, output_shape)
-        return ret
+		# transpose indices & reshape update values to one dimension
+		updates_size = tf.size(updates)
+		indices = tf.transpose(tf.reshape(
+			tf.stack([b, y, x, f]),
+			[4, updates_size]))
+		values = tf.reshape(updates, [updates_size])
+		ret = tf.scatter_nd(indices, values, output_shape)
+		return ret
 
-    def compute_output_shape(self, input_shape):
-        mask_shape = input_shape[1]
-        return (
-                mask_shape[0],
-                mask_shape[1]*self.size[0],
-                mask_shape[2]*self.size[1],
-                mask_shape[3]
-                )
+	def compute_output_shape(self, input_shape):
+		mask_shape = input_shape[1]
+		return (
+				mask_shape[0],
+				mask_shape[1]*self.size[0],
+				mask_shape[2]*self.size[1],
+				mask_shape[3]
+				)
 
 
 class SelfAttention(Layer):
-    '''
-    The self attention layer here is mainly for regularization
-    '''
-    def __init__(self,layers):
-        super(SelfAttention, self).__init__(autocast=False)
-        self.model = tf.keras.Sequential(layers)
+	'''
+	The self attention layer here is mainly for regularization
+	'''
+	def __init__(self,layers):
+		super(SelfAttention, self).__init__(autocast=False)
+		self.model = tf.keras.Sequential(layers)
 
-    def call(self,X):
-        attention_map = self.model(X)
-        return X*attention_map
+	def call(self,X):
+		attention_map = self.model(X)
+		return X*attention_map
 
-    def get_attention_map(self,X):
-        return self.model(X)
+	def get_attention_map(self,X):
+		return self.model(X)
 
 class ReshapeAndConcat(Layer):
-    '''
-    Reshapes and concats two inputs
-    Needed for the discriminator
-    '''
-    def __init__(self):
-        super(ReshapeAndConcat, self).__init__(autocast=False)
+	'''
+	Reshapes and concats two inputs
+	Needed for the discriminator
+	'''
+	def __init__(self):
+		super(ReshapeAndConcat, self).__init__(autocast=False)
 
-    def call(self,inputs):
-        X_inp,X_gen = inputs
-        bs,h,w,c = X_gen.shape
-        _,x1,x2,x3,x4 = X_inp.shape
-        #TODO ensure that shapes are compatible
-        #This is a very bad fix, atleast
-        # print(X_gen.dtype,X_inp.dtype)
-        X = tf.concat([tf.reshape(X_inp,(bs,h,w,(x1*x2*x3*x4)//(h*w))),tf.cast(X_gen,tf.float32)],axis=-1)
-        return X
+	def call(self,inputs):
+		X_inp,X_gen = inputs
+		bs,h,w,c = X_gen.shape
+		x1,x2,x3,x4 = X_inp.shape
+		#TODO ensure that shapes are compatible
+		#This is a very bad fix, atleast
+		# print(X_gen.dtype,X_inp.dtype)
+		X = tf.concat([tf.reshape(X_inp,(bs,h,w,(x2*x3*x4)//(h*w))),tf.cast(X_gen,tf.float32)],axis=-1)
+		return X
+
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 #TODO refactor to get argparse for models and dataset
-from model import MultiTaskModel
+from model_bits_and_pieces import MultiTaskModel
 from tqdm import tqdm
 
 import tensorflow.python.util.deprecation as deprecation
@@ -19,7 +19,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 for d in physical_devices:
-    tf.config.experimental.set_memory_growth(d, True)
+	tf.config.experimental.set_memory_growth(d, True)
 #tf.config.experimental.set_memory_growth(physical_devices[1], True)
 train_dir = 'data/'
 label_im_dir = 'label/images'
@@ -33,128 +33,129 @@ num_labels = 31
 sys.setrecursionlimit(10**6)
 
 def train(model,optimizer,epochs):  #TODO add validation, generator for datasets
-    train_files = []
-    val_files = []
-    for file in os.listdir(train_dir):
-        if file.endswith(".npy"):   #Change to h5 later
-            train_files.append(file)
-    for file in os.listdir(train_dir):
-        if file.endswith("v.npy"):   #Change to h5 later
-            val_files.append(file)
-    train_files = [x for x in train_files if x not in val_files]
-    log = open(log_file,"w")
-    for epoch in range(epochs):
-        print("Epoch num {}".format(epoch))
-        tloss = []
-        vloss = []
-        v_acc = 0
-        for f in tqdm(train_files):
-            input_batch = np.load("{}/{}".format(train_dir,f),allow_pickle=True)
-            label_batch = np.load("{}/{}".format(label_dir,f),allow_pickle=True)
-            label_im_batch = np.load("{}/{}".format(label_im_dir,f),allow_pickle=True)
-            l,l_all = model.train_on_batch(input_batch,label_im_batch,label_batch,optimizer)
-            tloss.append([l.numpy() for l in l_all])
-        for f in val_files:
-            input_batch = np.load("{}/{}".format(train_dir,f),allow_pickle=True)
-            label_batch = np.load("{}/{}".format(label_dir,f),allow_pickle=True)
-            label_im_batch = np.load("{}/{}".format(label_im_dir,f),allow_pickle=True)
-            l,l_all = model.validate_batch(input_batch,label_im_batch,label_batch)
-            v_acc += l 
-            sample = np.random.randint(15)
-            check = input_batch[:,sample:sample+1]
-            res = model.call(check)
-            fig, ax = plt.subplots(3, 3)
-            # print("Edges",np.max(res[3][0].numpy()),np.min(res[3][0].numpy()))
-            # print("Silhuette",np.max(res[0][0].numpy()),np.min(res[0][0].numpy()))
-            # print("Greyscale",np.max(res[2][0].numpy()),np.min(res[2][0].numpy()))
-            vloss.append([l.numpy() for l in l_all])
-            try:
-                ax[0,0].imshow(res[0][0])
-                ax[1,0].imshow(res[1][0])
-                ax[2,0].imshow(res[2][0])
-                ax[0,1].imshow(res[3][0])
-                ax[1,1].imshow(res[4][0])
-                ax[2,1].imshow(label_im_batch[sample])
-                ax[2,2].imshow(input_batch[:,sample][0])
-                ax[0,2].imshow(input_batch[:,sample][1])
-                ax[1,2].imshow(input_batch[:,sample][2])
-                plt.savefig("Reconstructed_Results/{}-{}.png".format(epoch,f))
-                plt.close()
-            except:
-                pass    
-            try:
-                fig, ax = plt.subplots(3, 5)
-                aten_rec,aten_pred = model.getAttentionMap(check)
-                for i in range(4):
-                    ax[0,i].imshow(aten_rec[i,0].mean(axis=0).reshape((32,32)))
-                    ax[1,i].imshow(aten_pred[i,0].mean(axis=0).reshape((32,32)))
-                    ax[2,i].imshow(res[i][0])
-                ax[0,4].imshow(label_im_batch[sample])
-                ax[1,4].imshow(res[4][0])
-                plt.savefig("AttentionMaps/{}-{}.png".format(epoch,f))
-                plt.close()
+	train_files = []
+	val_files = []
+	for file in os.listdir(train_dir):
+		if file.endswith(".npy"):   #Change to h5 later
+			train_files.append(file)
+	for file in os.listdir(train_dir):
+		if file.endswith("v.npy"):   #Change to h5 later
+			val_files.append(file)
+	train_files = [x for x in train_files if x not in val_files]
+	log = open(log_file,"w")
+	for epoch in range(epochs):
+		print("Epoch num {}".format(epoch))
+		tloss = []
+		vloss = []
+		v_acc = 0
+		for f in tqdm(train_files):
+			input_batch = np.load("{}/{}".format(train_dir,f),allow_pickle=True)
+			label_batch = np.load("{}/{}".format(label_dir,f),allow_pickle=True)
+			label_im_batch = np.load("{}/{}".format(label_im_dir,f),allow_pickle=True)
+			l,l_all = model.train_on_batch(input_batch,label_im_batch,label_batch,optimizer)
+			tloss.append([l.numpy() for l in l_all])
+		for f in val_files:
+			input_batch = np.load("{}/{}".format(train_dir,f),allow_pickle=True)
+			label_batch = np.load("{}/{}".format(label_dir,f),allow_pickle=True)
+			label_im_batch = np.load("{}/{}".format(label_im_dir,f),allow_pickle=True)
+			l,l_all = model.validate_batch(input_batch,label_im_batch,label_batch)
+			v_acc += l 
+			sample = np.random.randint(15)
+			check = input_batch[:,sample:sample+1]
+			res = model.call(check)
+			fig, ax = plt.subplots(3, 3)
+			# print("Edges",np.max(res[3][0].numpy()),np.min(res[3][0].numpy()))
+			# print("Silhuette",np.max(res[0][0].numpy()),np.min(res[0][0].numpy()))
+			# print("Greyscale",np.max(res[2][0].numpy()),np.min(res[2][0].numpy()))
+			vloss.append([l.numpy() for l in l_all])
+			try:
+				ax[0,0].imshow(res[0][0])
+				ax[1,0].imshow(res[1][0])
+				ax[2,0].imshow(res[2][0])
+				ax[0,1].imshow(res[3][0])
+				ax[1,1].imshow(res[4][0])
+				ax[2,1].imshow(label_im_batch[sample])
+				ax[2,2].imshow(input_batch[:,sample][0])
+				ax[0,2].imshow(input_batch[:,sample][1])
+				ax[1,2].imshow(input_batch[:,sample][2])
+				plt.savefig("Reconstructed_Results/{}-{}.png".format(epoch,f))
+				plt.close()
+			except:
+				pass    
+			try:
+				fig, ax = plt.subplots(3, 5)
+				aten_rec,aten_pred = model.getAttentionMap(check)
+				for i in range(4):
+					ax[0,i].imshow(aten_rec[i,0].mean(axis=0).reshape((32,32)))
+					ax[1,i].imshow(aten_pred[i,0].mean(axis=0).reshape((32,32)))
+					ax[2,i].imshow(res[i][0])
+				ax[0,4].imshow(label_im_batch[sample])
+				ax[1,4].imshow(res[4][0])
+				plt.savefig("AttentionMaps/{}-{}.png".format(epoch,f))
+				plt.close()
 
 
-            except:
-                pass        
+			except:
+				pass        
 
-        log.write("Epoch - {} Training_Loss - {} validation_loss - {},val_acc - {}".format(epoch,np.array(tloss).mean(axis=0),np.array(vloss).mean(axis=0),v_acc/(len(val_files)*16)))
-        log.write("\n")
-        print("Val-loss {}".format(np.array(vloss).mean(axis=0)[-1]))
-        if epoch % 5 == 0:
-            model.save('trained_models')
+		log.write("Epoch - {} Training_Loss - {} validation_loss - {},val_acc - {}".format(epoch,np.array(tloss).mean(axis=0),np.array(vloss).mean(axis=0),v_acc/(len(val_files)*16)))
+		log.write("\n")
+		print("Val-loss {}".format(np.array(vloss).mean(axis=0)[-1]))
+		if epoch % 5 == 0:
+			model.save('trained_models')
 
 if __name__ == "__main__":
-    TF_CPP_MIN_LOG_LEVEL = 4
-    parser = argparse.ArgumentParser()
+	TF_CPP_MIN_LOG_LEVEL = 4
+	parser = argparse.ArgumentParser()
 
-    #-db DATABSE -u USERNAME -p PASSWORD -size 20
-    parser.add_argument("-a","--attention", help="Using attention or not",action="store_true")
-    parser.add_argument("-p","--pix2pix", help="Using pix2pix",action="store_true")
-    parser.add_argument("-t","--twostage", help="Using two stage",action="store_true")
-    parser.add_argument("-s","--small", help="Small dataset",action="store_true")
+	#-db DATABSE -u USERNAME -p PASSWORD -size 20
+	parser.add_argument("-a","--attention", help="Using attention or not",action="store_true")
+	parser.add_argument("-p","--pix2pix", help="Using pix2pix",action="store_true")
+	parser.add_argument("-t","--twostage", help="Using two stage",action="store_true")
+	parser.add_argument("-s","--small", help="Small dataset",action="store_true")
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    # if(len(sys.argv)==2):
-    #   trainableVariablesFile = sys.argv[1]
-    #   trainableVariables = pickle.load(open(trainableVariablesFile,'rb'))
-    #   model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels,trainableVariables=trainableVariables)
-    # else:
-    #   model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels)
+	# if(len(sys.argv)==2):
+	#   trainableVariablesFile = sys.argv[1]
+	#   trainableVariables = pickle.load(open(trainableVariablesFile,'rb'))
+	#   model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels,trainableVariables=trainableVariables)
+	# else:
+	#   model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels)
 
-    if args.small:
-        train_dir = 'data_small/'
-        label_im_dir = 'label_small/images'
-        label_dir = 'label_small/labels'
-    model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels,attention=args.attention,pix2pix=args.pix2pix,two_stage=args.twostage)
-    
-    d1 = np.load("{}/0v.npy".format(train_dir),allow_pickle=True)
-    print(d1[:,:1].shape)
-    print("Building Model...")
-    model.build(d1[:,:1])
-    
-    # fig, ax = plt.subplots(3, 3)
-    # # print(res[0][0])
-    # # try:
-    # ax[0,0].imshow(res[0][0])
-    # ax[1,0].imshow(res[1][0])
-    # ax[2,0].imshow(res[2][0])
-    # ax[0,1].imshow(res[3][0])
-    # ax[1,1].imshow(res[4][0])
-    # # ax[2,1].imshow(label_im_batch[sample])
-    # ax[2,2].imshow(d1[:,0][0])
-    # ax[0,2].imshow(d1[:,0][1])
-    # ax[1,2].imshow(d1[:,0][2])
-    # plt.savefig("Reconstructed_Results/Init.png")
-    # plt.close()
-    # for x in res:
-    #   print("Shape",x.shape)  
-    print("Model built")
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-    model.setTrainableVariables()
-    train(model,optimizer,100)
-    # pred = model.call(d1)
+	if args.small:
+		train_dir = 'data_small/'
+		label_im_dir = 'label_small/images'
+		label_dir = 'label_small/labels'
+	model = MultiTaskModel(num_inputs=num_inputs,image_shape=image_shape,num_labels=num_labels,attention=args.attention,pix2pix=args.pix2pix,two_stage=args.twostage)
+	
+	d1 = np.load("{}/0v.npy".format(train_dir),allow_pickle=True)
+	print(d1[:,:1].shape)
+	print("Building Model...")
+	model.build(d1[:,:1])
+	
+	# fig, ax = plt.subplots(3, 3)
+	# # print(res[0][0])
+	# # try:
+	# ax[0,0].imshow(res[0][0])
+	# ax[1,0].imshow(res[1][0])
+	# ax[2,0].imshow(res[2][0])
+	# ax[0,1].imshow(res[3][0])
+	# ax[1,1].imshow(res[4][0])
+	# # ax[2,1].imshow(label_im_batch[sample])
+	# ax[2,2].imshow(d1[:,0][0])
+	# ax[0,2].imshow(d1[:,0][1])
+	# ax[1,2].imshow(d1[:,0][2])
+	# plt.savefig("Reconstructed_Results/Init.png")
+	# plt.close()
+	# for x in res:
+	#   print("Shape",x.shape)  
+	print("Model built")
+	optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+	model.setTrainableVariables()
+	train(model,optimizer,100)
+	# pred = model.call(d1)
+
 
 
 
